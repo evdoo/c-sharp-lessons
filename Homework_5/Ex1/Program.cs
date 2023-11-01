1d55b82d48e1016e4bc179da90b4a2223447d05d
Olga Evdokimova <o.evdokima@gmail.com>
2023-11-01 18:01:47 +0300

    [feat] добавлено ДЗ 5

---
+++ Homework_5/Ex1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Homework_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Привет! Введи любую строку (лучше с пробелами):");
+            Console.ResetColor();
+
+            string text = Console.ReadLine();
+
+            string[] wordsArray = SplitText(text);
+
+            PrintWords(wordsArray);
+        }
+
+        static string[] SplitText(string text)
+        {
+            if (text.Equals(""))
+            {
+                return new string[1] { "Ты ввел пустую строку. Слов в ней нет, выводить нечего" };
+            }
+            
+            int firstIndex = -1;
+            int lastIndex = -1;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!text[i].Equals(' '))
+                {
+                    firstIndex = i;
+                    break;
+                }
+            }
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                if (!text[i].Equals(' '))
+                {
+                    lastIndex = i;
+                    break;
+                }
+            }
+
+            int spaceCount = 0;
+            string trimText = "";
+
+            for (int i = firstIndex; i <= lastIndex; i++)
+            {
+                trimText += text[i];
+                
+                if (text[i].Equals(' ') && i - 1 >= 0 && !text[i - 1].Equals(' '))
+                {
+                    spaceCount++;
+                }
+            }
+
+            string[] words = new string[spaceCount + 1];
+            int arrayIndex = 0;
+            string word = "";
+
+            for (int i = 0; i < trimText.Length; i++)
+            {
+                if (!trimText[i].Equals(' '))
+                {
+                    word += trimText[i];
+                    if (i == trimText.Length - 1)
+                    {
+                        words[arrayIndex] = word;
+                    }
+                } else
+                {
+                     if (i - 1 >= 0 && !trimText[i - 1].Equals(' '))
+                    {
+                        words[arrayIndex] = word;
+                        word = "";
+                        arrayIndex++;
+                    }
+                }
+            }
+
+            return words;
+        }
+
+        static void PrintWords(string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ Homework_5/Ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Привет! Введи любое предложение:");
+            Console.ResetColor();
+
+            string text = Console.ReadLine();
+
+            string str = ReverseWords(text);
+
+            Console.WriteLine(str);
+        }
+
+        static string ReverseWords(string text)
+        {
+            string[] words = Split(text);
+            Array.Reverse(words);
+
+            return String.Join(" ", words);
+        }
+
+        static string[] Split(string text)
+        {
+            return text.Trim().Split(" ");
+        }
+    }
+}
